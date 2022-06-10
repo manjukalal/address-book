b1114b2ac98377eaebcbf83b08196cb14a8abec7
manjukalal <manjukalal1496@gmail.com>
2022-06-10 11:42:33 +0530

    Added:logic for create contacts

---
+++ src/Contact.java
@@ -0,0 +1,104 @@
+public class Contact {
+    public Contact() {
+    }
+
+    public Contact(String firstname, String lastname, String address, String city, String state, int zipcode, String email, int mobile) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zipcode = zipcode;
+        this.email = email;
+        this.mobile = mobile;
+    }
+
+    private String firstname;
+    private String lastname;
+    private String address;
+    private String city;
+    private String state;
+    private int zipcode;
+    private String email;
+    private int mobile;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public int getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(int zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(int mobile) {
+        this.mobile = mobile;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", address='" + address + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zipcode=" + zipcode +
+                ", email='" + email + '\'' +
+                ", mobile=" + mobile +
+                '}';
+    }
+}
+
+
